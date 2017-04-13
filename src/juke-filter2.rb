6f57368a1439f8951c9af98201689e05e353bbfe
Jeongmin Ju <mini666@daum.net>
2017-04-13 16:18:36 +0900

    gsub 이용

--- src/juke-filter2.rb
+++ src/juke-filter2.rb
@@ -1,20 +1,13 @@
 #!/usr/bin/env ruby
 
-def substude(str)
-  if str.to_i.odd?
-    '*' * str.length
-  else
-    '#' * str.length
-  end
-end
-
 while (line = gets)
-  loop do
-    match_data = line.match(/[0-9]+/)
-    break unless match_data
-    result = substude(match_data[0])
-    line = line.sub(match_data[0], result)
+  result = line.gsub(/([0-9]+)/) do |s|
+    if $1.to_i.odd?
+      '*' * $1.length
+    else
+      '#' * $1.length
+    end
   end
 
-  puts line
+  puts result
 end
