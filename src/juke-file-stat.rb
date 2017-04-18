abcde2aea0a45a30a01f09cd22690aa161e1832a
Jeongmin Ju <mini666@daum.net>
2017-04-18 09:12:36 +0900

    Update juke-file-stat.rb
    코드 간소화

--- src/juke-file-stat.rb
+++ src/juke-file-stat.rb
@@ -11,30 +11,12 @@ ARGV.each do |path|
   end
 end
 
-def collect_file_stat(file_stat, file)
-  file_stat[:count] += 1
-  file_size = File.size(file)
-  file_stat[:sum] += file_size
-  file_stat[:max] = file_stat[:max] < file_size ? file_size : file_stat[:max]
-  file_stat[:min] = file_stat[:min].zero? ? file_size : file_stat[:min] > file_size ? file_size : file_stat[:min]
-  file_stat[:avg] = file_stat[:sum] / (file_stat[:count] * 1.0)
-  file_stat
-end
-
 ext_groups = files.group_by { |file| File.extname(file) }
 file_stats = ext_groups.map do |entry|
-  init_obj = { count: 0, sum: 0, max: 0, min: 0, avg: 0 }
-  [entry[0], entry[1].reduce(init_obj) do |file_stat, file|
-    collect_file_stat(file_stat, file)
-  end]
+  [entry[0], entry[1].map { |file| File.size(file) }]
 end
 
 file_stats.to_h.sort.each do |entry|
-  printf("[%s]\n", entry[0])
-  printf("-count: %d\n", entry[1][:count])
-  printf("-sum(size): %d\n", entry[1][:sum])
-  printf("-max(size): %d\n", entry[1][:max])
-  printf("-min(size): %d\n", entry[1][:min])
-  printf("-avg(size): %f\n", entry[1][:avg])
-  printf("\n")
+  printf("[%s]\n-count: %d\n-sum(size): %d\n-max(size): %d\n-min(size): %d\n-avg(size): %f\n\n",
+         entry[0], entry[1].count, entry[1].sum, entry[1].max, entry[1].min, entry[1].sum / (entry[1].count * 1.0))
 end
