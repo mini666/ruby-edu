9aee14f60248b97c13814b67c197aa8ddc6bc89f
Jeongmin Ju <mini666@daum.net>
2017-04-19 09:10:39 +0900

    Update juke-file-stat.rb
    코드 간소화

--- src/juke-file-stat.rb
+++ src/juke-file-stat.rb
@@ -1,22 +1,21 @@
 #!/usr/bin/env ruby
 
-files = []
-ARGV.each do |path|
+files = ARGV.flat_map do |path|
   if Dir.exist?(path)
-    files += Dir["#{path}/*"]
+    Dir["#{path}/*"]
   elsif File.exist?(path)
-    files << path
+    path
   else
-    puts "#{path} is not directory or file."
-  end
-end
+    nil 
+  end 
+end.compact
 
 ext_groups = files.group_by { |file| File.extname(file) }
-file_stats = ext_groups.map do |entry|
-  [entry[0], entry[1].map { |file| File.size(file) }]
+file_stats = ext_groups.map do |ext, items|
+  [ext, items.map { |file| File.size(file) }]
 end
 
-file_stats.to_h.sort.each do |entry|
+file_stats.to_h.sort.each do |ext, sizes|
   printf("[%s]\n-count: %d\n-sum(size): %d\n-max(size): %d\n-min(size): %d\n-avg(size): %f\n\n",
-         entry[0], entry[1].count, entry[1].sum, entry[1].max, entry[1].min, entry[1].sum / (entry[1].count * 1.0))
+         ext, sizes.count, sizes.sum, sizes.max, sizes.min, sizes.sum / (sizes.count * 1.0))
 end
