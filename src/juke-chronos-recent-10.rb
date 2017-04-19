f61290a6cc2dc4c085c665b3a40050cf89049f0c
Jeongmin Ju <mini666@daum.net>
2017-04-19 09:19:22 +0900

    Update juke-chronos-recent-10.rb
    sort를 sort_by로 변경

--- src/juke-chronos-recent-10.rb
+++ src/juke-chronos-recent-10.rb
@@ -5,10 +5,9 @@ require 'json'
 
 chronos_url = 'http://xxx.xxx.xxx/xxx/xxx'
 json = JSON.parse(open(chronos_url).read, symbolize_names: true)
-sorted_items = json.sort do |a, b|
-  a_time = a[:lastSuccess].empty? ? Time.at(0) : Time.parse(a[:lastSuccess])
-  b_time = b[:lastSuccess].empty? ? Time.at(0) : Time.parse(b[:lastSuccess])
-  b_time <=> a_time
+sorted_items = json.sort_by do |item|
+  lastSuccessTime = item[:lastSuccess]
+  (lastSuccessTime.empty? ? Time.at(0).to_i : Time.parse(lastSuccessTime).to_i) * -1
 end
 
 top_10 = sorted_items.take(10)
